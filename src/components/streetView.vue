d850ded34797738cff2385f02619cb28c8fc182e
ChrisAyo <Chris.ayodele01@gmail.com>
2021-03-19 17:33:23 +0000

    WIP Streetview google static image component: git status

---
+++ src/components/streetView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <v-img
+      height="150"
+      src="https://maps.googleapis.com/maps/api/streetview?location=41.403609,2.174448&size=456x456&key=AIzaSyAWCeHVGAhiySpUN9nKx7hV-b1yRL-QtMk"
+    >
+    </v-img>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({}),
+  props: {},
+  methods: {},
+};
+</script>
+
